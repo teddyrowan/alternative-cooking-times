dddb812216575ae8c9ada1870bde35be817d1e0d
Teddy Rowan <teddy@teddyrowan.com>
2019-12-28 15:06:32 -0500

    realized old shit was wrong solution to ODE, started playing around with proper one

--- alternate_cooking_temp.py
+++ alternate_cooking_temp.py
@@ -20,6 +20,26 @@
 # how to solve DE....
 # qqq* dt = dT/(T_oven - T)^4
 
+
+#### original solution is wrong, i must be an idiot / transfered poorly to WA
+# this is the actual solution.
+# https://www.wolframalpha.com/input/?i=alpha*dt+%3D+dT%2F%28215+-+T%29%5E4
+# T(t) = 215 - (2*qqq*c1*t + c1^2 + qqq^2*t^2)^(1/3) / (3^(1/3)*(c1 + qqq*t))
+# Apply BCs
+# T(0) = -15
+# https://www.wolframalpha.com/input/?i=215+-+%28c1%5E2%29%5E%281%2F3%29+%2F+%283%5E%281%2F3%29*c1%29+%3D+-15
+# T(0) = 215 - (c1^2)^(1/3) / (3^(1/3)*c1) = -15
+# c1 = 2.73965096*10^-8
+# T(20) = 75
+# https://www.wolframalpha.com/input/?i=215+-+%282*x*%282.73965096*10%5E-8%29*20+%2B+%282.73965096*10%5E-8%29%5E2+%2B+x%5E2*20%5E2%29%5E%281%2F3%29+%2F+%283%5E%281%2F3%29*%28%282.73965096*10%5E-8%29+%2B+x*20%29%29+%3D+75
+# qqq ≈ -1.36983×10^-9 or ≈ 4.70403×10^-9
+# should be the positive root?
+
+# now plug that back into solve for the lower temp
+
+
+#### This shit is just plain fucking wrong dumbshit.
+
 # with T_oven = 215.
 # T(t) = c1*exp(-qqq*t) + 215
 # T(0) = -15
