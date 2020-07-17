5d0682318062fde21fd383df43bb96826e09a8fe
TR <teddy@teddyrowan.com>
2020-07-17 18:52:19 -0400

    now prompts user for temperatures and then also produces a plot after

--- README.md
+++ README.md
@@ -8,34 +8,28 @@ The package uses Euler's Method...
 
 Blebber blab blob
 
-<img src="https://latex.codecogs.com/gif.latex?x=\frac{-b\pm\sqrt{b^2-4ac}}{2a}" title="x=\frac{-b\pm\sqrt{b^2-4ac}}{2a}" />
+== example inline equations w/ latex renderer: https://www.codecogs.com/latex/eqneditor.php
+
 
 
 ### Walking through the algorithm 
 
 The Governing Equation:
 
-```
-dQ/dt = m*Cv*(dT) = a*(Ts^4 - T^4)
-```
+<img src="https://latex.codecogs.com/gif.latex?dQ/dt=mC_vdT=A\sigma(Ts^4-T^4)" title="dQ/dt=mC_vdT=A\sigma(Ts^4-T^4)" />
+
 
 This differential equation unfortunately doesn't have a particularly clean solution. So let's use Euler's Method and find a numerical approximation to the solution.
 
-
-```
-dQ/dT(n) = m*Cv*(T(n+1) - T(n)) = a*(Ts^4 - T(n)^4)
-```
-
 Grouping constants and re-arranging: 
 
-```
-T(n+i)  = T(n) + c1*(Ts^4 - T(n)^4)
-T(n+2i) = T(n+i) + c1*(Ts^4 - T(n+i)^4)
-        = T(n) + c1*(Ts^4 - T(n)^4) + c1*(Ts^4 - (T(n) + c1*(Ts^4 - T(n)^4))^4)
-        = T(n) + 2*c1*Ts^4 - c1*T(n)^4 - c1*(T(n) + c1*(Ts^4 - T(n)^4))^4
-```
+<img src="https://latex.codecogs.com/gif.latex?%5Cbegin%7Balign*%7D%20T%28n&plus;1i%29%20%26%3D%20T%28n%29%20&plus;%20c1*%28Ts%5E4%20-%20T%28n%29%5E4%29%20%5C%5C%20T%28n&plus;2i%29%20%26%3D%20T%28n&plus;i%29%20&plus;%20c1*%28Ts%5E4%20-%20T%28n&plus;i%29%5E4%29%20%5C%5C%20%26%3D%20T%28n%29%20&plus;%202*c1*Ts%5E4%20-%20c1*T%28n%29%5E4%20-%20c1*%28T%28n%29%20&plus;%20c1*%28Ts%5E4%20-%20T%28n%29%5E4%29%29%5E4%20%5Cend%7B%7D" title="\begin{align*} T(n+i) &= T(n) + c1*(Ts^4 - T(n)^4) \\ T(n+2i) &= T(n+i) + c1*(Ts^4 - T(n+i)^4) \\ &= T(n) + 2*c1*Ts^4 - c1*T(n)^4 - c1*(T(n) + c1*(Ts^4 - T(n)^4))^4 \end{}" />
 
+From experience, c1 will tend to be on the order of 10^-10, so rather than solving a 5th degree polynomial, we can eliminate higher power of c1 terms and find the following approximation:
 
+<img src="https://latex.codecogs.com/gif.latex?c1\approx1/2*\frac{T(n&plus;2i)-T(n)}{Ts^4-T(n)^4}" title="c1\approx1/2*\frac{T(n+2i)-T(n)}{Ts^4-T(n)^4}" />
+
+Now we can use this c1 value and plug in values for the new temperature settings and step through using Euler's Method until we reach the final temperature. This method will inevitably step past the desired temperature, but we can then interpolate between the final two steps to find an approximate new cooking time. 
 
 ### Author
 * Teddy Rowan
--- alternate_temp_solver.py
+++ alternate_temp_solver.py
@@ -1,112 +1,72 @@
-#alternate_temp_solver.py
+# alternate_temp_solver.py
 # The actual solver accompagnying alternate_cooking_temp.py
 # Author: Teddy Rowan
 
-# For now I'm approximating 0C = 273K (dropping the .15)
+import matplotlib.pyplot as plt
+
+def calculate_cook_time(start_temp, end_temp, hot_oven_temp, cold_oven_temp, original_time):
+    c1 = (end_temp-start_temp)/(pow(hot_oven_temp,4) - pow(start_temp,4))/2 # first power of c1 approximation (scale E-10)
+    
+    current_temp = start_temp
+    temp_history = [current_temp]
+
+    while current_temp < end_temp: # basic Euler's Method step-through. 
+        next_temp = current_temp + c1*(pow(cold_oven_temp,4) - pow(current_temp,4))
+        temp_history.append(next_temp)
+        current_temp = next_temp
+
+    interp = (TF - temp_history[-1])/(temp_history[-2] - temp_history[-1])
+    steps = (len(temp_history) - 1) - interp
+
+    # now figure out the length of the steps. ie: original cook time / 2  and then multiply by the step size
+    time_total = steps*original_time/2
+    return time_total
+    
 
 # Example:
 # Chicken Strips take 20 minutes to cook at 425 F = 215 C = 488 K
 # Freezer is at about -15C ie: initial temp of the chicken = 258 K
 # The chicken needs to get cooked to 75 C = 348 K
 # If you decide to cook the chicken at 300 F = 150 C = 423 K, how long should it take
+#T0         = -15 + k_convert;  # 258 K Initial Chicken Temperature
+#TF         =  75 + k_convert;  # 348 K Final Chicken Tmeperature
+#TSH        = 215 + k_convert;  # 488 K Hot version of the oven
+#time_orig  = 20                # original cooking time. 
+#TSC        = 150 + k_convert;  # 423 K Cold version of the oven
 
-T0  = -15 + 273;    # 258 K Initial Chicken Temperature
-TF  =  75 + 273;    # 348 K Final Chicken Tmeperature
-TSC = 150 + 273;    # 423 K Cold version of the oven
-TSH = 215 + 273;    # 488 K Hot version of the oven
-time_orig = 20;
+# Option to list safe internal temperatures of meats.
+k_convert = 273.15;     # conversion from celcius to kelvin
 
-c1 = (TF-T0)/(pow(TSH,4) - pow(T0,4))/2 # first power of c1 approximation
-#print(c1) #8.60720 e-10
+T0          = float(input('Enter the intial temperature of the food (freezer ~ -15, fridge ~ 0C) [Celcius]: ')) + k_convert
+TF          = float(input('Enter the final cooking temperature of the food [Celcius]: ')) + k_convert
+TSH         = float(input('Enter the recommended cooking temperature for the food [Celcius]: ')) + k_convert
+time_orig   = float(input('Enter the recommended cooking time @ previous temperature [minutes]: '))
+TSC         = float(input('At what temperature would you like to cook the food [Celcius]: ')) + k_convert
 
-track = [T0]
-temp = T0
-
-while temp < TF: # basic Euler's Method step-through. 
-    temp_n = temp + c1*(pow(TSC,4) - pow(temp,4))
-    track.append(temp_n)
-    temp = temp_n
-
-#print(track)
-
-# now interpolate. solve this.
-# TF = x*track[len(track)-2] + (1-x)*track[len(track)-1]
-interp = (TF - track[len(track)-1])/(track[len(track)-2] - track[len(track)-1])
-steps = (len(track) - 1) - interp
-#print(steps) #good. 
-
-# now figure out the length of the steps. ie: original cook time / 2
-# then multiply the step
-time_total = steps*time_orig/2;
-#print("Cook time at alternate temperature: " + str(time_total) + " minutes.")
-print("Cook time at alternate temperature: %02d mins." % time_total)
+new_time = calculate_cook_time(T0, TF, TSH, TSC, time_orig)
+print("Cook time at alternate temperature: %02d mins." % new_time)
 
 
-# below this is explanations. 
+## Now let's loop through and find a curve. 
+low = 100 + k_convert;
+high = 500 + k_convert;
 
+temp_list = []
+time_list = []
 
-# The Governing Equation:
-# dQ/dt = m*Cv*(dT) = a*(Ts^4 - T^4)
-# This differential equation unfortunately doesn't have a particularly clean solution
+temp = low
+while temp < high:
+    tmp_time = calculate_cook_time(T0, TF, TSH, temp, time_orig)
+    temp_list.append(temp)
+    time_list.append(tmp_time)
+    temp = temp + 5
 
-# Let's use the known values and use Euler's Method to approximate the solution
-# dQ/dT(n) = m*Cv*(T(n+1) - T(n)) = a*(Ts^4 - T(n)^4)
-# Grouping Constants:
-# T(n+i)  = T(n) + c1*(Ts^4 - T(n)^4)
-# T(n+2i) = T(n+i) + c1*(Ts^4 - T(n+i)^4)
-#         = T(n) + c1*(Ts^4 - T(n)^4) + c1*(Ts^4 - (T(n) + c1*(Ts^4 - T(n)^4))^4)
-#         = T(n) + 2*c1*Ts^4 - c1*T(n)^4 - c1*(T(n) + c1*(Ts^4 - T(n)^4))^4
+temp_list = [x - k_convert for x in temp_list]
 
-#TF = T0 + 2*c1*TSH^4 - c1*T0^4 - c1*(T0 + c1*(TSH^4 - T0^4))^4
-#TF - T0 = c1(2*TSH^4 - T0^4 - (T0 + c1*(TSH^4 - T0^4))^4)
-#x = TF- T0 || y = 2*TSH^4 - T0^4 || z = T0 || r = (TSH^4 - T0^4)
-#x = c1*(y - (z + c1*r)^4) # will need to do some root-finding for this to solve c1.  (x,y,z,r are just numbers)
-#x = c1*(y - (z + c1*r)^4)
-## ohhhhh since the numbers are so small, can probably taylor-approximate and clear out the first few terms
-#x = c1*y - c1*(z^2 + 2*z*c1*r + c1^2*r^2)^2
-#x ~ c1*y - c1*z^4 = c1*(2*TSH^4 - T0^4 - T0^4)
-#c1 ~ = (TF-T0)/(2*TSH^4 - T0^4 - T0^4)
-#c1 ~ = (90)/(2*488^4 - 2*258^4) = 8.60720 * 10^-10 # off by 4%  # good enough for now. worst case use as initial approx.
-
-
-# hmmmm is it worth looking at another term? probably not. complex roots. i guess not then. 
-# the next hanging terms would be...
-#x = c1*y - c1*(z^2 + 2*z*c1*r + O(c1^2))^2
-#x ~ c1*y - c1*(z^4 + 4*z^5*c1*r + O(c1^2))
-#x ~ c1*y - c1*z^4 - 4*z^5*r*c1^2
-#0 ~ c1(y-z^4) - c1^2*(4*z^5*r) - x
-#0 ~ c1*((2*TSH^4 - T0^4)-T0^4) - c1^2*(4*T0^5*(TSH^4 - T0^4)) - (TF-T0)
-#0 ~ c1*((2*488^4 - 258^4)-258^4) - c1^2*(4*258^5*(488^4 - 258^4)) - (90) #c1 is complex. 
-
-# Example: (using 10 minute steps)
-# T(2) = T(0) + 2*c1*Ts^4 - c1*T(0)^4 - c1*(T(0) + c1*(Ts^4 - T(0)^4))^4
-# 348 = 258 + 2*c1*(488)^4 - c1*(258)^4 - c1*(258 + c1*(488^4 - 258^4))^4
-# Solved on Wolfram: c1 = 8.9683E-10 or 5.6279E-9
-# This is the one step that will be a pain to do programatically. Start off with just a binomial search between 0-1 and then after that move to NM or similar to converge faster. 
-
-
-
-# Plug c1 into the colder temperature example and see what we get. 
-#T(i+1) = T(i)      + c1*(T_s^4 - T(i)^4)
-#T(i+1) = T(i)      + 8.9683*10^-10*(423^4 - T(i)^4)
-#T(1)   = 258       + 8.9683*10^-10*(423^4 - 258.00^4) = 282.74 K = 9.74  C
-#T(2)   = 282.74    + 8.9683*10^-10*(423^4 - 282.74^4) = 305.72 K = 32.72 C
-#T(3)   = 305.72    + 8.9683*10^-10*(423^4 - 305.72^4) = 326.60 K = 53.00 C
-#T(4)   = 326.00    + 8.9683*10^-10*(423^4 - 326.00^4) = 344.58 K = 71.58 C
-#T(5)   = 344.58    + 8.9683*10^-10*(423^4 - 344.58^4) = 360.65 K = 87.65 C
-
-# Interpolating between T(4) and T(5) to find T(x) = 75 C
-# T(x) = 4.21 ==> It should take 42 minutes to cook the chicken strips @ 300 F instead of 425 F
-# Euler's Method in this case is a faster than actual approximation due to the declining nature of the DE.
-
-## What happens if we use the other root of c1?
-#T(1)   = 258       + 5.6279*10^-9*(423^4 - 258.00^4) = 413.24 K = 140 C  --> basically already converged. 
-
-## What happens if we use the approximate solution of c1?
-#T(1)   = 258       + 8.5623*10^-10(423^4 - 258.00^4) = 281.62 K = 8.62  C
-#T(2)   = 281.62    + 8.5623*10^-10(423^4 - 281.62^4) = 303.64 K = 30.64 C
-#T(3)   = 303.64    + 8.5623*10^-10(423^4 - 303.64^4) = 323.77 K = 50.77 C
-#T(4)   = 323.77    + 8.5623*10^-10(423^4 - 323.77^4) = 341.77 K = 68.77 C
-#T(5)   = 341.77    + 8.5623*10^-10(423^4 - 341.77^4) = 357.50 K = 84.50 C
-# Interpolating 4.39 ==> 44 minutes to cook the chicken @ 300 F instead of 425 F
-# Close enough for an early run. My fear is that this might become an issue as I expand. 
+fig = plt.figure()
+plt.plot(temp_list, time_list, 'ro-', markersize=3)
+plt.xlabel("Oven Temperature [Celcius]")
+plt.ylabel("Equivalent Cooking Time [minutes]")
+plt.title("Alternative Cooking Times")
+plt.grid()
+plt.show()
