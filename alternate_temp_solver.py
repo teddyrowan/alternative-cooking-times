4ab0d8cedf534609ad52fe9fc6dc05d0ddc73af0
TR <teddy@teddyrowan.com>
2020-07-17 01:00:01 -0400

    worked out and checked the math on c1 approximations instead of solving 5th degree polynomial

--- alternate_temp_solver.py
+++ alternate_temp_solver.py
@@ -11,6 +11,12 @@
 # The chicken needs to get cooked to 75 C = 348 K
 # If you decide to cook the chicken at 300 F = 150 C = 423 K, how long should it take
 
+T0  = 15 + 273;     # 258 K Initial Chicken Temperature
+TF  = 75 + 273;     # 348 K Final Chicken Tmeperature
+TSC = 150 + 273;    # 423 K Cold version of the oven
+TSH = 215 + 273;    # 488 K Hot version of the oven
+
+
 # The Governing Equation:
 # dQ/dt = m*Cv*(dT) = a*(Ts^4 - T^4)
 # This differential equation unfortunately doesn't have a particularly clean solution
@@ -22,6 +28,26 @@
 # T(n+2i) = T(n+i) + c1*(Ts^4 - T(n+i)^4)
 #         = T(n) + c1*(Ts^4 - T(n)^4) + c1*(Ts^4 - (T(n) + c1*(Ts^4 - T(n)^4))^4)
 #         = T(n) + 2*c1*Ts^4 - c1*T(n)^4 - c1*(T(n) + c1*(Ts^4 - T(n)^4))^4
+
+TF = T0 + 2*c1*TSH^4 - c1*T0^4 - c1*(T0 + c1*(TSH^4 - T0^4))^4
+TF - T0 = c1(2*TSH^4 - T0^4 - (T0 + c1*(TSH^4 - T0^4))^4)
+x = TF- T0 || y = 2*TSH^4 - T0^4 || z = T0 || r = (TSH^4 - T0^4)
+x = c1*(y - (z + c1*r)^4) # will need to do some root-finding for this to solve c1.  (x,y,z,r are just numbers)
+x = c1*(y - (z + c1*r)^4)
+# ohhhhh since the numbers are so small, can probably taylor-approximate and clear out the first few terms
+x = c1*y - c1*(z^2 + 2*z*c1*r + c1^2*r^2)^2
+x ~ c1*y - c1*z^4 = c1*(2*TSH^4 - T0^4 - T0^4)
+c1 ~ = (TF-T0)/(2*TSH^4 - T0^4 - T0^4)
+c1 ~ = (90)/(2*488^4 - 2*254^4) = 8.5623 * 10^-10 # off by 5% 
+
+# hmmmm is it worth looking at another term? probably not. complex roots. i guess not then. 
+# the next hanging terms would be...
+x = c1*y - c1*(z^2 + 2*z*c1*r + O(c1^2))^2
+x ~ c1*y - c1*(z^4 + 4*z^5*c1*r + O(c1^2))
+x ~ c1*y - c1*z^4 - 4*z^5*r*c1^2
+0 ~ c1(y-z^4) - c1^2*(4*z^5*r) - x
+0 ~ c1*((2*TSH^4 - T0^4)-T0^4) - c1^2*(4*T0^5*(TSH^4 - T0^4)) - (TF-T0)
+0 ~ c1*((2*488^4 - 258^4)-258^4) - c1^2*(4*258^5*(488^4 - 258^4)) - (90) #c1 is complex. 
 
 # Example: (using 10 minute steps)
 # T(2) = T(0) + 2*c1*Ts^4 - c1*T(0)^4 - c1*(T(0) + c1*(Ts^4 - T(0)^4))^4
@@ -43,3 +69,14 @@
 # T(x) = 4.21 ==> It should take 42 minutes to cook the chicken strips @ 300 F instead of 425 F
 # Euler's Method in this case is a faster than actual approximation due to the declining nature of the DE.
 
+## What happens if we use the other root of c1?
+#T(1)   = 258       + 5.6279*10^-9*(423^4 - 258.00^4) = 413.24 K = 140 C  --> basically already converged. 
+
+## What happens if we use the approximate solution of c1?
+#T(1)   = 258       + 8.5623*10^-10(423^4 - 258.00^4) = 281.62 K = 8.62  C
+#T(2)   = 281.62    + 8.5623*10^-10(423^4 - 281.62^4) = 303.64 K = 30.64 C
+#T(3)   = 303.64    + 8.5623*10^-10(423^4 - 303.64^4) = 323.77 K = 50.77 C
+#T(4)   = 323.77    + 8.5623*10^-10(423^4 - 323.77^4) = 341.77 K = 68.77 C
+#T(5)   = 341.77    + 8.5623*10^-10(423^4 - 341.77^4) = 357.50 K = 84.50 C
+# Interpolating 4.39 ==> 44 minutes to cook the chicken @ 300 F instead of 425 F
+# Close enough for an early run. My fear is that this might become an issue as I expand. 
